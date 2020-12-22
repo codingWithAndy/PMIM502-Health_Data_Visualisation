64a4ea5d6c8bf7321f6dc0ee581a7f8375b2a562
Andy Gray <30691631+codingWithAndy@users.noreply.github.com>
2020-12-22 18:29:31 +0000

    ONS file imported.

--- Assignment/HCDV Assignment/Exporing the datasets..R
+++ Assignment/HCDV Assignment/Exporing the datasets..R
@@ -5,3 +5,12 @@ mydata
 car_data = read.csv("Road Safety Data- Vehicles 2019.csv")
 
 car_data
+install.packages("readODS")
+library(readODS)
+ods_data = read.ods(file = '3275f503-b71f-47db-9b00-81a88c6202b0.ods', sheet = "GB - All accidents", formulaAsFormula = FALSE)
+
+# ONS Sheets
+# GB - All accidents
+# Countries - All accidents
+# Regions - All accidents
+ods_data
