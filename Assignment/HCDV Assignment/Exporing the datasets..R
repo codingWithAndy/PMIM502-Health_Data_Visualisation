f967d486a6ba1bc9d511de0faddbc510ae3e0039
Andy Gray <30691631+codingWithAndy@users.noreply.github.com>
2020-12-28 15:17:41 +0000

    Poster development.

--- Assignment/HCDV Assignment/Exporing the datasets..R
+++ Assignment/HCDV Assignment/Exporing the datasets..R
@@ -1,3 +1,24 @@
+install.packages("gapminder")
+library(gapminder)
+
+install.packages("dplyr")
+library(dplyr)
+
+install.packages("ggplot2")
+library(ggplot2)
+
+install.packages("Cairo")
+library(Cairo)
+
+install.packages("plotly")
+library(plotly)
+
+install.packages("scales")
+library(scales)
+
+install.packages("extrafont")
+library(extrafont)
+
 mydata = read.csv("Road Safety Data - Casualties 2019.csv")
 
 mydata
@@ -7,6 +28,7 @@ car_data = read.csv("Road Safety Data- Vehicles 2019.csv")
 car_data
 install.packages("readODS")
 library(readODS)
+library(ggplot2)
 ods_data = read.ods(file = '3275f503-b71f-47db-9b00-81a88c6202b0.ods', sheet = "GB - All accidents", formulaAsFormula = FALSE)
 
 # ONS Sheets
@@ -14,3 +36,11 @@ ods_data = read.ods(file = '3275f503-b71f-47db-9b00-81a88c6202b0.ods', sheet = "
 # Countries - All accidents
 # Regions - All accidents
 ods_data
+
+wales_data =  read.ods(file = 'Wales info.ods', sheet = 2, formulaAsFormula = FALSE)
+
+wales_data
+
+ggplot(wales_data %>% filter(Local_authority=="Conwy"),aes(x=Light_condition
+                                                 ,y=All_accidents
+                                                 ,colour=Light_condition))
